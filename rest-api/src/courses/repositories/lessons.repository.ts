43e545f9c9cdcb0d4c0f8c7062c1c912b405680c
Philip Lysenko <philip.lysenko.extern@sda.se>
2020-03-15 21:40:04 +0100

    fixed schema property mismatch bug

--- rest-api/src/courses/repositories/lessons.repository.ts
+++ rest-api/src/courses/repositories/lessons.repository.ts
@@ -9,15 +9,18 @@ export class LessonsRepository {
     @InjectModel('Lesson') private lessonsModel: Model<Lesson>
   ) {}
 
+  // if find does not produce results, the Schema might be wrong,
+  // it seems it does not verify that a param does not exist
   public searchLessons(
     courseId: string,
     sortOrder: "asc" | "desc",
     pageNumber: number,
     pageSize: number
   ): Promise<Lesson> {
-    console.log("searching for lessons: " + courseId, sortOrder, pageNumber, pageSize);
+    console.log("searching for lessons: ", courseId, sortOrder, pageNumber, pageSize);
     return this.lessonsModel.find(
       // filter criteria (more possible):
+      // better set this to Partial<Lesson> to avoid property mismatch
       {course: courseId},
       // fields to return; null for all
       null,
--- rest-api/src/courses/schemas/lessons.schema.ts
+++ rest-api/src/courses/schemas/lessons.schema.ts
@@ -3,12 +3,9 @@ import * as mongoose from 'mongoose';
 export const LessonsSchema = new mongoose.Schema({
   // the real object also has the Mongo-typical _id property, here it is implicit
   description: String,
-  duration: Number,
-  seqNo: {
-    type: Number,
-    required: true
-  },
-  courseId: { // one to many relationship by object-identifier
+  duration: String,
+  seqNo: Number,
+  course: { // one to many relationship by object-identifier
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Course'
   }
